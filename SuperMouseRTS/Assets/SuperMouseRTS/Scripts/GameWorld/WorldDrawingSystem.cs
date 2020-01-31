014ecf17bac231262855b255ae8ba1649c4f818c
Maqqr <rinne.simo@gmail.com>
2020-02-01 00:26:05 +0200

    Basic tile drawing works now

--- SuperMouseRTS/Assets/SuperMouseRTS/Scripts/GameWorld/WorldDrawingSystem.cs
+++ SuperMouseRTS/Assets/SuperMouseRTS/Scripts/GameWorld/WorldDrawingSystem.cs
@@ -20,7 +20,20 @@ namespace Assets.SuperMouseRTS.Scripts.GameWorld
         //    }
         //}
 
+        /// <summary>
+        /// This is constructed from the tile's prefab representation.
+        /// </summary>
+        private struct ProcessedMesh
+        {
+            public Mesh Mesh;
+            public Material Material;
+            public Matrix4x4 Transform;
+        }
+
+        // This might not be needed anymore
         Dictionary<TileContent, GameObject> tilePrefabs = new Dictionary<TileContent, GameObject>();
+
+        Dictionary<TileContent, ProcessedMesh[]> tileMeshes = new Dictionary<TileContent, ProcessedMesh[]>();
 
         protected override void OnCreate()
         {
@@ -42,6 +55,19 @@ namespace Assets.SuperMouseRTS.Scripts.GameWorld
             foreach (var pair in GameManager.Instance.LoadedSettings.TilePrefabs)
             {
                 tilePrefabs.Add(pair.TileContent, pair.TilePrefab);
+
+                var meshes = new List<ProcessedMesh>();
+                foreach (var renderer in pair.TilePrefab.GetComponentsInChildren<MeshRenderer>())
+                {
+                    meshes.Add(new ProcessedMesh()
+                    {
+                        Mesh = renderer.GetComponent<MeshFilter>().sharedMesh,
+                        Material = renderer.sharedMaterial,
+                        Transform = renderer.GetComponent<Transform>().localToWorldMatrix,
+                    });
+                }
+
+                tileMeshes.Add(pair.TileContent, meshes.ToArray());
             }
         }
 
@@ -49,22 +75,30 @@ namespace Assets.SuperMouseRTS.Scripts.GameWorld
         {
             Entities.ForEach((ref Tile tile, ref TilePosition position) =>
             {
-                if (tilePrefabs.TryGetValue(tile.tile, out GameObject prefab))
+                if (tileMeshes.TryGetValue(tile.tile, out ProcessedMesh[] meshes))
                 {
-                    foreach (var renderer in prefab.GetComponentsInChildren<MeshRenderer>())
+                    var tileOrigin = new Vector3(position.Position.x, 0f, position.Position.y);
+                    var tileMatrix = Matrix4x4.Translate(tileOrigin);
+
+                    foreach (var mesh in meshes)
                     {
-                        var meshFilter = renderer.GetComponent<MeshFilter>();
-                        var material = renderer.sharedMaterial;
-
-                        Vector3 localPosition = renderer.GetComponent<Transform>().localPosition;
-                        Vector3 tileOrigin = new Vector3(position.Position.x, 0f, position.Position.y);
-
-                        Vector3 finalPosition = tileOrigin + localPosition;
-                        var modelMatrix = Matrix4x4.Translate(finalPosition);
-
-                        Graphics.DrawMesh(meshFilter.sharedMesh, modelMatrix, material, 0);
+                        Graphics.DrawMesh(mesh.Mesh, tileMatrix * mesh.Transform, mesh.Material, 0);
                     }
                 }
+
+                //if (tilePrefabs.TryGetValue(tile.tile, out GameObject prefab))
+                //{
+                //    foreach (var renderer in prefab.GetComponentsInChildren<MeshRenderer>())
+                //    {
+                //        var meshFilter = renderer.GetComponent<MeshFilter>();
+                //        var material = renderer.sharedMaterial;
+
+                //        var prefabMatrix = renderer.GetComponent<Transform>().localToWorldMatrix;
+                //        var modelMatrix = Matrix4x4.Translate(new Vector3(position.Position.x, 0f, position.Position.y)) * prefabMatrix;
+
+                //        Graphics.DrawMesh(meshFilter.sharedMesh, modelMatrix, material, 0);
+                //    }
+                //}
             });
         }
     }
