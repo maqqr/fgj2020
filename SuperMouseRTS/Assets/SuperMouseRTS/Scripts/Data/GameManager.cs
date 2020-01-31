a8fee0ffda404945a31908eec058c10ce561376f
Tero Paavolainen <teropaav@hotmail.fi>
2020-01-31 19:18:45 +0200

    Started first system and settings and game manager

---
+++ SuperMouseRTS/Assets/SuperMouseRTS/Scripts/Data/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AddressableAssets;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    private const string settingsDataAddress = "Settings";
+
+    private Settings loadedSettings;
+    public Settings LoadedSettings
+    {
+        get
+        {
+            return loadedSettings;
+        }
+    }
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Addressables.LoadAssetsAsync<Settings>(settingsDataAddress, SettingsLoaded);
+
+    }
+
+    private void SettingsLoaded(Settings obj)
+    {
+        var settings = obj as Settings;
+
+        if(settings == null)
+        {
+            throw new UnityException("Hammer time!");
+        }
+
+        loadedSettings = settings;
+    }
+}
