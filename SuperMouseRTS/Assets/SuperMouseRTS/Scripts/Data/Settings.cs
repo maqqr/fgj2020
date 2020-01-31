9b847e763b8f515688a085985e5dcee9d55fa7d3
Maqqr <rinne.simo@gmail.com>
2020-01-31 23:39:46 +0200

    Started working on world drawing

--- SuperMouseRTS/Assets/SuperMouseRTS/Scripts/Data/Settings.cs
+++ SuperMouseRTS/Assets/SuperMouseRTS/Scripts/Data/Settings.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
+public struct TilePrefabPair
+{
+    public Assets.SuperMouseRTS.Scripts.GameWorld.TileContent TileContent;
+    public GameObject TilePrefab;
+}
+
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Create Settings ScriptableObject", order = 1)]
 public class Settings : ScriptableObject
 {
     public int tilesHorizontally = 15;
     public int tilesVertically = 10;
 
+    public TilePrefabPair[] TilePrefabs;
 }
--- SuperMouseRTS/Assets/SuperMouseRTS/Scripts/GameWorld/WorldDrawingSystem.cs
+++ SuperMouseRTS/Assets/SuperMouseRTS/Scripts/GameWorld/WorldDrawingSystem.cs
@@ -1,5 +1,7 @@
-﻿using Unity.Burst;
+﻿using UnityEngine;
+using Unity.Burst;
 using Unity.Collections;
+using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
@@ -8,56 +10,62 @@ using static Unity.Mathematics.math;
 
 namespace Assets.SuperMouseRTS.Scripts.GameWorld
 {
-    public class WorldDrawingSystem : JobComponentSystem
+    public class WorldDrawingSystem : ComponentSystem
     {
+        //[BurstCompile]
+        //struct WorldDrawingSystemJob : IJobForEach<Translation, Rotation>
+        //{
+        //    public void Execute(ref Translation translation, [ReadOnly] ref Rotation rotation)
+        //    {
+        //    }
+        //}
 
+        Dictionary<TileContent, GameObject> tilePrefabs = new Dictionary<TileContent, GameObject>();
 
-
-        // This declares a new kind of job, which is a unit of work to do.
-        // The job is declared as an IJobForEach<Translation, Rotation>,
-        // meaning it will process all entities in the world that have both
-        // Translation and Rotation components. Change it to process the component
-        // types you want.
-        //
-        // The job is also tagged with the BurstCompile attribute, which means
-        // that the Burst compiler will optimize it for the best performance.
-        [BurstCompile]
-        struct WorldDrawingSystemJob : IJobForEach<Translation, Rotation>
+        protected override void OnCreate()
         {
-            // Add fields here that your job needs to do its work.
-            // For example,
-            //    public float deltaTime;
-
-
-
-            public void Execute(ref Translation translation, [ReadOnly] ref Rotation rotation)
+            Enabled = GameManager.Instance.IsSettingsLoaded;
+            if (Enabled)
             {
-                // Implement the work to perform for each entity here.
-                // You should only access data that is local or that is a
-                // field on this job. Note that the 'rotation' parameter is
-                // marked as [ReadOnly], which means it cannot be modified,
-                // but allows this job to run in parallel with other jobs
-                // that want to read Rotation component data.
-                // For example,
-                //     translation.Value += mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
+                Initialize();
+            }
 
+            GameManager.Instance.OnSettingsLoaded += delegate
+            {
+                Enabled = true;
+                Initialize();
+            };
+        }
 
+        private void Initialize()
+        {
+            foreach (var pair in GameManager.Instance.LoadedSettings.TilePrefabs)
+            {
+                tilePrefabs.Add(pair.TileContent, pair.TilePrefab);
             }
         }
 
-        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        protected override void OnUpdate()
         {
-            var job = new WorldDrawingSystemJob();
+            Entities.ForEach((ref Tile tile, ref TilePosition position) =>
+            {
+                if (tilePrefabs.TryGetValue(tile.tile, out GameObject prefab))
+                {
+                    foreach (var renderer in prefab.GetComponentsInChildren<MeshRenderer>())
+                    {
+                        var meshFilter = renderer.GetComponent<MeshFilter>();
+                        var material = renderer.sharedMaterial;
 
-            // Assign values to the fields on your job here, so that it has
-            // everything it needs to do its work when it runs later.
-            // For example,
-            //     job.deltaTime = UnityEngine.Time.deltaTime;
+                        Vector3 localPosition = renderer.GetComponent<Transform>().localPosition;
+                        Vector3 tileOrigin = new Vector3(position.Position.x, 0f, position.Position.y);
 
+                        Vector3 finalPosition = tileOrigin + localPosition;
+                        var modelMatrix = Matrix4x4.Translate(finalPosition);
 
-
-            // Now that the job is set up, schedule it to be run. 
-            return job.Schedule(this, inputDependencies);
+                        Graphics.DrawMesh(meshFilter.sharedMesh, modelMatrix, material, 0);
+                    }
+                }
+            });
         }
     }
 }
