8c58e94ef3bf69ebd427f42d3a261064c62bedbf
Simo Rinne <rinne.simo@gmail.com>
2020-10-11 11:09:42 +0300

    Smooth rotation for units

--- SuperMouseRTS/Assets/SuperMouseRTS/Scripts/Players/Units/MovementSystem.cs
+++ SuperMouseRTS/Assets/SuperMouseRTS/Scripts/Players/Units/MovementSystem.cs
@@ -18,10 +18,14 @@ public class MovementSystem : JobComponentSystem
 
         public void Execute(Entity ent, int index, ref Translation translation, ref Rotation rotation, [ReadOnly] ref MovementSpeed speed, [ReadOnly] ref NearestUnit nearest)
         {
-            translation.Value += WorldCoordinateTools.UnityCoordinateAsWorld(speed.Value.x, speed.Value.y) * DeltaTime; ;
+            translation.Value += WorldCoordinateTools.UnityCoordinateAsWorld(speed.Value.x, speed.Value.y) * DeltaTime;
 
-            float angle = atan2(speed.Value.y, speed.Value.x) - PI * 0.5f;
-            rotation.Value = EulerAngle(0f, angle, 0f);
+            var currentAngle = ToEulerAngles(rotation.Value).y;
+            var targetAngle = atan2(speed.Value.y, speed.Value.x) - PI * 0.5f;
+
+            currentAngle += (targetAngle - currentAngle) * DeltaTime * 2.0f;
+
+            rotation.Value = EulerAngle(0f, currentAngle, 0f);
         }
 
         public quaternion EulerAngle(float yaw, float pitch, float roll) // yaw (Z), pitch (Y), roll (X)
@@ -40,6 +44,35 @@ public class MovementSystem : JobComponentSystem
             q.value.z = sy * cp * cr - cy * sp * sr;
 
             return q;
+        }
+
+        float3 ToEulerAngles(quaternion quat)
+        {
+            float4 q = quat.value;
+            float3 angles;
+
+            // roll (x-axis rotation)
+            float sinr_cosp = 2 * (q.w * q.x + q.y * q.z);
+            float cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y);
+            angles.x = atan2(sinr_cosp, cosr_cosp);
+
+            // pitch (y-axis rotation)
+            float sinp = 2 * (q.w * q.y - q.z * q.x);
+            if (abs(sinp) >= 1)
+            {
+                angles.y = PI / 2 * sign(sinp); // use 90 degrees if out of range
+            }
+            else
+            {
+                angles.y = asin(sinp);
+            }
+
+            // yaw (z-axis rotation)
+            float siny_cosp = 2 * (q.w * q.z + q.x * q.y);
+            float cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
+            angles.z = atan2(siny_cosp, cosy_cosp);
+
+            return angles;
         }
     }
 
